7dd828aa8cc635c166e2f9db2a925f367ce73e09
Sylvain <syl@thinkpad.localhost>
2024-11-17 13:55:16 +0100

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,11 +4,11 @@
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
 
-sudo_path = $(which sudo)
-f2bc_path = $(which fail2ban-client)
-grep_path = $(which grep)
-awk_path = $(which awk)
-fail2ban_sock = "/var/run/fail2ban/fail2ban.sock"
+sudo_path=$(which sudo)
+f2bc_path=$(which fail2ban-client)
+grep_path=$(which grep)
+awk_path=$(which awk)
+fail2ban_sock="/var/run/fail2ban/fail2ban.sock"
 
 myynh_set_permissions () {
 	chown -R "$app:www-data" "$install_dir"
